57b07c4b8b9229a6671807fb90407e2e239646e0
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-06-16 23:00:10 +0400

    Update start.py

--- SakkuMusic/utils/inline/start.py
+++ SakkuMusic/utils/inline/start.py
@@ -32,5 +32,6 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
+        [InlineKeyboardButton(text="Geliştirici👨🏻‍💻", user_id=6436992668)],
     ]
     return buttons
